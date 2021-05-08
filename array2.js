27738a6ad8f670c3f7d1ebb12cfbd75a8097896f
serhat <serhat06.sc@gmail.com>
2021-05-07 19:18:01 -0700

    arrays 2 second week

---
+++ array2.js
@@ -0,0 +1,269 @@
+// //arry sort() includes() reverse() join(),slice() lastIndexOf, findIndex,  find,
+// filter, Set
+
+//object inside of array//  array inside array 
+
+
+
+
+// let fruit=["orange ","apple","banana","civi","grapes","melon"];
+
+
+
+// console.log(fruit);
+
+// order by acsending 
+// fruit.sort();
+// console.log(fruit);
+
+// fruit.push("watermelon");
+// fruit.push("pineapple");
+
+// console.log(fruit);
+
+// fruit.sort();
+// console.log(fruit);
+
+
+
+/// check it for numbers
+
+// var num=[5,10,50,60,35,23];
+
+// console.log(num);
+
+
+//     num.sort();
+
+//       console.log(num);
+
+// num.reverse();
+// console.log(num);
+
+
+
+//  let fruit=["orange ","apple","banana","civi","grapes","melon"];
+
+
+
+// console.log(fruit);
+
+// fruit.sort();
+
+// console.log(fruit);
+
+// fruit.reverse();
+
+// console.log(fruit);
+
+
+// let arr=[
+  
+//   5,
+//   10,
+//   "javastring",
+//   {
+//     name:"serhat",
+//     id:101
+  
+    
+//   }
+//   ,{
+    
+//     name:"ali",
+//     surname:"yildiz"
+//   }
+  
+  
+//   //json 
+// ]
+
+// console.log(arr);
+
+// console.log(typeof(arr));
+
+// console.log(typeof(arr[0]));
+
+// console.log(typeof(arr[2]));
+
+// console.log(typeof(arr[3]));
+
+// console.log(arr[3]);
+
+// console.log(arr[3].name);
+
+// console.log(arr[3][1]);
+
+
+// we can add array inside of arraay
+
+// let arr=[30,
+//          "alican",
+//          [1,2,3],
+//          ["halil","levent","isa"],
+//          {name:"asli",id:101}
+//         ];
+
+
+// console.log(arr);
+
+
+
+// we can use index number for reach the inside of array
+
+
+
+
+// console.log(arr[2]);
+
+
+// we can use double index for reach the  inside nested array
+
+
+// console.log(arr[2][1]);
+
+// console.log(arr[4].name);
+
+// arr.sort();
+// console.log(arr);
+// arr[2].sort();
+
+// // sort() for inside of array check it
+
+
+// console.log(arr);
+
+
+
+//includes  use for check elements it is existing or not returning tru or false
+
+
+// let person=["ali","pinar","john","bush"];
+
+// console.log(person.includes("ali"));
+
+// console.log(person.includes("capasule"));
+
+
+// console.log(person.includes("bush"));
+
+
+
+//join use for remove averthing between items and put what you want
+
+// let arr=["car","computer","laptop","pc","plane"];
+
+// console.log(arr);
+
+// console.log(arr.join());
+            
+// console.log(arr.join("  "));
+
+
+
+//slice slice(index no) start from index no and take elemnets up to end of array
+
+//slice(starting index,finishing index) taking elements from starting index up to finish ndex 
+     //    includes,excludes
+
+// let car=["toyota","bmw","mercedes","audia","nissan","mazda"];
+
+
+// console.log(car);
+// console.log(car.slice(1));
+// console.log(car.slice(2));
+// console.log(car.slice(2,4));
+// console.log(car.slice(2,5));
+
+
+//lastIndexOf(element) given us index of last element
+
+
+// let arr=["orange","apple","banana","apple","melon","apple"];
+
+// console.log(arr);
+// console.log(arr.indexOf("apple")); //returnig index no
+// console.log(arr.indexOf("orange"));
+
+// console.log(arr.lastIndexOf("apple"));
+
+
+
+//findIndex
+
+
+var num=[1,4,60,130,55,85,57];
+
+console.log(num);
+
+var num1=(item)=>item>50;
+
+
+
+console.log(num.findIndex(num1));
+
+//console.log(arr.findIndex(55));
+
+
+
+
+// console.log(num.findIndex(function num1(item){
+  
+//   return item==55;
+  
+// }));
+
+
+
+// //console.log(num.findIndex(55));
+
+
+// let ar= (item) =>item==60;
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
