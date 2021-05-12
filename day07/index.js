0ce1b58fc804820810fcdff93408e29d38713e52
serhat <serhat06.sc@gmail.com>
2021-05-11 22:06:50 -0700

    day07

---
+++ day07/index.js
@@ -0,0 +1,16 @@
+
+
+function evenOdd(){
+
+let num=document.getElementById("number").value;
+
+if(num%2===0){
+
+    alert("this number is even number "+num);
+} else{
+alert("this number is odd "+num);
+
+}
+
+
+}
